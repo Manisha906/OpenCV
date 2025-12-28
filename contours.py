b57bf5200208ff15727e0a4eefd6f484065dc0b9
Manisha Papineni <manishapapineni1@gmail.com>
2025-12-28 15:46:08 +0530

    Update contours.py

--- contours.py
+++ contours.py
@@ -3,7 +3,7 @@ img=cv2.imread("opencv/download.png")
 gray=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 ret,thresh=cv2.threshold(gray,120,255,cv2.THRESH_BINARY)
 contours,hierarchy=cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
-# cv2.drawContours(img,contours,-1,(0,255,0),3)
+cv2.drawContours(img,contours,-1,(0,255,0),3)
 for contour in contours:
     approx=cv2.approxPolyDP(contour,0.01*cv2.arcLength(contour,True),True)
     cv2.drawContours(img,[approx],0,(0,255,0),2)
